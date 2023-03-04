e6e301f14128b8f2f4532a98ac7aa4d00289072d
John Wehrle <wehrlej@gmail.com>
2023-03-03 17:37:04 -0800

    Upgraded packages

--- lib/widgets/editor/text_selection.dart
+++ lib/widgets/editor/text_selection.dart
@@ -4,7 +4,7 @@ import 'dart:math' as math;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+// import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 
 import 'package:flutter_quill/models/documents/nodes/node.dart';
